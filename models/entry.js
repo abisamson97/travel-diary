ee8ad5ab435a5c3d0cd35b35ee95817c8ed94bdb
Abi Samson <abi.samson97@yahoo.com>
2023-10-30 15:46:33 -0400

    entry schema model created

--- models/entry.js
+++ models/entry.js
@@ -2,4 +2,13 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const entrySchema = new Schema 
+const entrySchema = new Schema({
+    location: {type: String, required: true},
+    lodging: {type: String},
+    activities: {type: String},
+    restaurants: {type: String}
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Entry', entrySchema);
