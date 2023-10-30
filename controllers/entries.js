7903f8f8e5ba8b3aa8ea8050fabdbf57e1db1d34
Abi Samson <abi.samson97@yahoo.com>
2023-10-30 17:13:54 -0400

    index controller function for list of trips

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,8 +1,14 @@
 const Entry = require('../models/entry');
 
 module.exports = {
-    new: newEntry
+    new: newEntry,
+    index
 }
+
+async function index(req, res) {
+    const entries = await Entry.find({});
+    res.render('entries/index', { entries });
+  }
 
 function newEntry(req, res) {
     res.render('entries/new', { title: 'Add Trip', errorMsg: ''});
