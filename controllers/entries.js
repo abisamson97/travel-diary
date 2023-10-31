19ff8f2ac82bf17de67934965327c6716a0084ba
Abi Samson <abi.samson97@yahoo.com>
2023-10-31 10:29:43 -0400

    entry create controller function defined

--- controllers/entries.js
+++ controllers/entries.js
@@ -2,9 +2,20 @@ const Entry = require('../models/entry');
 
 module.exports = {
     new: newEntry,
-    index
+    index,
+    create
 }
 
+async function create(req, res) {
+    try {
+        await Entry.create(req.body);
+        res.redirect('/entries/new');
+      } catch (err) {
+        console.log(err);
+        res.render('entries/new', { errorMsg: err.message });
+      }
+    }
+    
 async function index(req, res) {
     const entries = await Entry.find({});
     res.render('entries/index', { entries });
